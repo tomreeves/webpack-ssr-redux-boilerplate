1b86db5e5edce18b2faacd809f2ea03a9ab67ed4
tomreeves <tom.reeves@hotmail.co.uk>
2020-02-13 10:14:57 +0000

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Home from './client/App/containers/Home.jsx'
 
+/**
+ * These routes are required server side. We match the path to a
+ * given component, and check if the component has any data needs.
+ *
+ * If the component has any 'needs' (essentially a Redux action),
+ * we dispatch the 'need' and send the data down to the client as part
+ * of the initial Redux store.
+ */
 const routes = [
   {
     path: '/',
