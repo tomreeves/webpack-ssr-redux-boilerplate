4b8b958cef1b736be02e3b8f59661f6395f5244c
tomreeves <tom.reeves@hotmail.co.uk>
2020-02-20 10:26:54 +0000

    Update compRoutes.js

--- src/shared/routes/compRoutes.js
+++ src/shared/routes/compRoutes.js
@@ -8,12 +8,10 @@ const Test = loadable(() =>
 )
 
 /**
- * These routes are required server side. We match the path to a
- * given component, and check if the component has any data needs.
+ * Client: mapped in the React Router <Switch />
  *
- * If the component has any 'needs' (essentially a Redux action),
- * we dispatch the 'need' and send the data down to the client as part
- * of the initial Redux store.
+ * Server: matched against the incoming request path
+ *         to get the data needs 
  */
 const routes = [
   {
